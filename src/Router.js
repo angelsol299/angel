7b9914709e471e52ab43a5cde32b54836570380d
angelsol299 <angelsol299@hotmail.com>
2018-11-21 21:43:01 +0100

    router

---
+++ src/Router.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+const Router = () => (
+  <Switch>
+    <Route exat path='/ component={HomePage"' />
+  </Switch>
+);
+
+export default Router;
