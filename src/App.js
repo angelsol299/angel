6b2f9261d1cf19130df0ab8f4b9b5995343b7277
angelsol299 <angelsol299@hotmail.com>
2018-11-21 21:59:50 +0100

    homepage.js with some content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Router from "./Router";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>hello world</h1>
+        <Router />
       </div>
     );
   }
