d3396fe046cfd4cb2e4a922ff09e7a9024e5d7af
angelsol299 <angelsol299@hotmail.com>
2018-11-23 23:24:16 +0100

    completing cart reducer

--- src/features/cart/reducer.js
+++ src/features/cart/reducer.js
@@ -1,3 +1,12 @@
+const cartWithoutItem = (cart, item) => cart.filter(cartItem => cartItem.id !== item.id)
+const itemInCart = (cart, item) => cart.filter(cartItem => cartItem.id === item.id)[0]
+
+const addToCart = (cart, item) => {
+    const cartItem = itemcart(cart, item)
+    return cartItem === undefined
+    ? [...cartWithoutItem(cart, item), {...item, quantity: 1}]
+}
+
 const cartReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD":
